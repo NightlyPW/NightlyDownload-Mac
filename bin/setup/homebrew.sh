fa9d6b815e27c9f890d0ebe0b9c90786f0ca8a1d
NightlyPW <humphreyoliver2@gmail.com>
2017-11-22 21:31:45 +0000

    Added WGET & HOMEBREW download.

---
+++ bin/setup/homebrew.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo Installing Homebrew for NightlyDownload (Mac).
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+echo Installed Homebrew, you may now use NightlyDownload without any known problems.
---
+++ bin/wget/wget.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo Installing WGET...
+brew install wget
+echo Installed WGET. (You may now run the NightlyDownload.)
